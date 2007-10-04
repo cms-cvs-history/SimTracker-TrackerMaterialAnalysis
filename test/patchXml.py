891545e7b2537eaef34027f6e24529af0e1fc508
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-10-04 20:57:49 +0000

    First published version

---
+++ test/patchXml.py
@@ -0,0 +1,29 @@
+#! /bin/env python2.4
+
+import os
+from xml.dom.minidom import parse
+
+rads = []
+dedx = []
+
+# parse data file
+data = open('parameters')
+lines = data.readlines()
+for line in lines:
+  tokens = line.split()
+  rads.append( float(tokens[6]) )
+  dedx.append( float(tokens[9]) )
+
+# parse xml file
+dom = parse(os.environ['CMSSW_RELEASE_BASE'] + '/src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml')
+layers = dom.getElementsByTagName('SpecPar')
+
+for layer in layers:
+  parameters = layer.getElementsByTagName('Parameter')
+  for parameter in parameters:
+    if parameter.getAttribute('name') == u'TrackerRadLength':
+      parameter.setAttribute('value', str(rads.pop(0)))
+    if parameter.getAttribute('name') == u'TrackerXi':
+      parameter.setAttribute('value', str(dedx.pop(0)))
+
+print dom.toxml()
