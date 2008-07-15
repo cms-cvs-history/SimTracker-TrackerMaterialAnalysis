4a59fd99f8419d649df48535c8ba462d446c83f9
cmsbuild <>
2008-07-15 14:53:36 +0000

    This commit was manufactured by cvs2git to create branch 'SimTracker- TrackerMaterialAnalysis-B01-06-00'.
    Sprout from master 2008-07-09 21:30:24 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'pythonization of .cfg files'
    Cherrypick from master 2008-07-15 14:53:35 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'add missing import for Geometry and Magnetic Field':
        plugins/TrackingMaterialAnalyser.cc
        plugins/TrackingMaterialPlotter.cc
        python/trackingMaterialAnalyser_cff.py
        python/trackingMaterialProducer_cff.py
    Cherrypick from master 2008-01-16 14:25:54 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'move RandomNumberGeneratorService to its own .cfi':
        data/randomNumberGeneratorService.cfi
        plugins/BuildFile

--- python/trackingMaterialProducer_cff.py
+++ python/trackingMaterialProducer_cff.py
@@ -1,9 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Geometry and Magnetic Field
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
-from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
+from Configuration.StandardSequences.Geometry_cff import *
+from Configuration.StandardSequences.MagneticField_40T_cff import *
 from SimTracker.TrackerMaterialAnalysis.randomNumberGeneratorService_cfi import *
 from SimTracker.TrackerMaterialAnalysis.trackingMaterialProducer_cfi import *
-
